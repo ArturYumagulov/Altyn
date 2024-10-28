bf39459cea8692cef1d4388b0de821b1de85c6a8
arturumagulov <zico.13288@gmail.com>
2024-10-28 22:09:08 +0300

    1. Закончил верстку, перехожу к сбору шаблонов

--- regions/views.py
+++ regions/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from regions.models import Region, ScreeningPoint, RegionalInternetResources
+from regions.models import Region, ScreeningPoint, RegionalInternetResources, Production
 
 
 # Create your views here.
@@ -16,10 +16,12 @@ def main(requests):
 
     if len(requests.GET) > 0:
         region = Region.objects.get(slug=requests.GET.get('region'))
-        screening_points = ScreeningPoint.objects.filter(is_active=True, region=region)
-        internet_resources = RegionalInternetResources.objects.filter(is_active=True, region=region)
+        screening_points = ScreeningPoint.objects.filter(is_active=True, region=region)[:2]
+        internet_resources = RegionalInternetResources.objects.filter(is_active=True, region=region)[:2]
+        productions = Production.objects.filter(is_active=True, region=region)[:2]
 
-        context['screening_points'] = screening_points[:2]
-        context['internet_resources'] = internet_resources[:2]
+        context['screening_points'] = screening_points
+        context['internet_resources'] = internet_resources
+        context['productions'] = productions
 
     return render(requests, 'regions/main.html', context=context)
